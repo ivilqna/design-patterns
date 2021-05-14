c231e3149a04ac14e0ec2a76f85e6c0ca176eb9e
ivilqna <59650456+ivilqna@users.noreply.github.com>
2021-05-14 17:08:53 +0300

    Add files via upload

---
+++ design-patterns/src/abstractFactory/AbstractFactoryMain.java
@@ -0,0 +1,18 @@
+package abstractFactory;
+
+public class AbstractFactoryMain {
+
+	public static void main(String[] args) {
+
+
+		AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+		Shape circle = shapeFactory.getShape("CIRCLE");
+		circle.draw();
+		
+		AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
+		Color blue = colorFactory.getColor("BLUE");
+		blue.fill();
+
+	}
+
+}
