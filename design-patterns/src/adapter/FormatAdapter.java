68b4951d6e9f7aa3b8024525f4e462c67047eb6a
ivilqna <59650456+ivilqna@users.noreply.github.com>
2021-06-12 12:57:37 +0300

    Add files via upload

---
+++ design-patterns/src/adapter/FormatAdapter.java
@@ -0,0 +1,16 @@
+package adapter;
+
+public class FormatAdapter implements MediaPlayer {
+
+	private MediaPackage mediaPackage;
+	
+	public FormatAdapter(MediaPackage mediaPackage) {
+	this.mediaPackage = mediaPackage;
+	}
+	
+	public void play(String fileName) {
+		System.out.println("Opening file with an adapter");
+		this.mediaPackage.playFile(fileName);
+	}
+
+}
